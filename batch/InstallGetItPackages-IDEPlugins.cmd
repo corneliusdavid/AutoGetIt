72a629c1a25284ac3ac90b6d8d40c7f06b3428b5
dev@corneliusconcepts.com <corneliusdavid>
2020-09-25 18:44:03 -0700

    until I get the app working, these batch files will work

---
+++ batch/InstallGetItPackages-IDEPlugins.cmd
@@ -0,0 +1,3 @@
+getitcmd -ae -i="Bookmarks-1.6.2"
+getitcmd -ae -i="CodeSite-5.3.4"
+getitcmd -ae -i="Navigator-1.6.1"
---
+++ batch/UninstallGetItPackages-IDEPlugins.cmd
@@ -0,0 +1,3 @@
+getitcmd -u="Bookmarks-1.6.2"
+getitcmd -u="CodeSite-5.3.4"
+getitcmd -u="Navigator-1.6.1"
