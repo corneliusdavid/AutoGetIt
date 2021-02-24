5ac88be42a57f4f20828966bca4dcf60cda2825c
dev@corneliusconcepts.com <corneliusdavid>
2021-02-24 13:02:42 -0800

    improvements to parsing the DosCommand text, added sorting/filtering options

--- ufrmAutoGetItMain.dfm
+++ ufrmAutoGetItMain.dfm
@@ -3,10 +3,10 @@ object frmAutoGetItMain: TfrmAutoGetItMain
   Top = 0
   Caption = 'Auto GetIt!'
   ClientHeight = 569
-  ClientWidth = 848
+  ClientWidth = 852
   Color = clBtnFace
   Constraints.MinHeight = 300
-  Constraints.MinWidth = 480
+  Constraints.MinWidth = 868
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -13
@@ -19,49 +19,83 @@ object frmAutoGetItMain: TfrmAutoGetItMain
   object pnlTop: TPanel
     Left = 0
     Top = 0
-    Width = 848
-    Height = 81
+    Width = 852
+    Height = 89
     Align = alTop
     TabOrder = 0
-    ExplicitWidth = 549
+    ExplicitWidth = 848
     DesignSize = (
-      848
-      81)
+      852
+      89)
     object Label1: TLabel
-      Left = 529
+      Left = 532
       Top = 22
       Width = 116
       Height = 16
       Alignment = taRightJustify
       Anchors = [akTop, akRight]
       Caption = 'RAD Studio Version:'
-      ExplicitLeft = 280
+      ExplicitLeft = 550
     end
     object btnRefresh: TBitBtn
       Left = 8
       Top = 17
       Width = 169
-      Height = 39
+      Height = 56
       Action = actRefresh
       Caption = '&Refresh Package List'
       TabOrder = 0
     end
     object cmbRADVersions: TComboBox
-      Left = 651
+      Left = 654
       Top = 19
-      Width = 185
+      Width = 186
       Height = 24
       Style = csDropDownList
       Anchors = [akTop, akRight]
       TabOrder = 1
-      ExplicitLeft = 402
+      ExplicitLeft = 704
+    end
+    object rgrpSortBy: TRadioGroup
+      Left = 183
+      Top = 10
+      Width = 210
+      Height = 46
+      Caption = 'Sort By'
+      Columns = 3
+      ItemIndex = 0
+      Items.Strings = (
+        'Name'
+        'Vendor'
+        'Date')
+      TabOrder = 2
+    end
+    object chkInstalledOnly: TCheckBox
+      Left = 416
+      Top = 28
+      Width = 97
+      Height = 17
+      Caption = 'Installed Only'
+      TabOrder = 3
+    end
+    object edtNameFilter: TLabeledEdit
+      Left = 272
+      Top = 62
+      Width = 265
+      Height = 24
+      EditLabel.Width = 89
+      EditLabel.Height = 16
+      EditLabel.Caption = 'Name contains:'
+      LabelPosition = lpLeft
+      TabOrder = 4
+      TextHint = 'leave blank to include all packages'
     end
   end
   object lbPackages: TCheckListBox
     Left = 0
-    Top = 81
-    Width = 848
-    Height = 488
+    Top = 89
+    Width = 852
+    Height = 461
     Align = alClient
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
@@ -75,7 +109,28 @@ object frmAutoGetItMain: TfrmAutoGetItMain
       'three')
     ParentFont = False
     TabOrder = 1
-    ExplicitWidth = 281
+    ExplicitTop = 81
+    ExplicitWidth = 848
+    ExplicitHeight = 488
+  end
+  object StatusBar: TStatusBar
+    Left = 0
+    Top = 550
+    Width = 852
+    Height = 19
+    Panels = <
+      item
+        Width = 100
+      end
+      item
+        Width = 100
+      end
+      item
+        Width = 50
+      end>
+    ExplicitLeft = 432
+    ExplicitTop = 296
+    ExplicitWidth = 0
   end
   object aclAutoGetit: TActionList
     Left = 216
--- ufrmAutoGetItMain.pas
+++ ufrmAutoGetItMain.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Actions, Vcl.ActnList,
   System.ImageList, Vcl.ImgList, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls,
-  DosCommand, Vcl.CheckLst;
+  DosCommand, Vcl.CheckLst, Vcl.ComCtrls;
 
 type
   TfrmAutoGetItMain = class(TForm)
@@ -19,6 +19,10 @@ type
     DosCommand: TDosCommand;
     lbPackages: TCheckListBox;
     actInstallChecked: TAction;
+    rgrpSortBy: TRadioGroup;
+    chkInstalledOnly: TCheckBox;
+    edtNameFilter: TLabeledEdit;
+    StatusBar: TStatusBar;
     procedure actRefreshExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure DosCommandNewLine(ASender: TObject; const ANewLine: string; AOutputType: TOutputType);
@@ -28,11 +32,16 @@ type
     const
       BDS_USER_ROOT = '\Software\Embarcadero\BDS\';
     var
-      PastFirstItem: Boolean;
-      Finished: Boolean;
+      FPastFirstItem: Boolean;
+      FFinished: Boolean;
+    procedure SetDownloadTime(const Value: Integer);
+    procedure SetPackageCount(const Value: Integer);
     procedure LoadRADVersionsCombo;
+    procedure CleanPackageList;
     function BDSRootPath(const BDSVersion: string): string;
     function SelectedBDSVersion: string;
+    property PackageCount: Integer write SetPackageCount;
+    property DownloadTime: Integer write SetDownloadTime;
   end;
 
 var
@@ -44,7 +53,13 @@ implementation
 {$R *.dfm}
 
 uses
-  System.Win.Registry, System.StrUtils;
+  System.Diagnostics, System.Win.Registry, System.StrUtils;
+
+procedure TfrmAutoGetItMain.FormCreate(Sender: TObject);
+begin
+  LoadRADVersionsCombo;
+  lbPackages.Items.Clear;
+end;
 
 procedure TfrmAutoGetItMain.actInstallCheckedExecute(Sender: TObject);
 begin
@@ -64,21 +79,37 @@ begin
 end;
 
 procedure TfrmAutoGetItMain.actRefreshExecute(Sender: TObject);
+var
+  SortField: string;
 begin
   lbPackages.Items.Clear;
-  PastFirstItem := False;
-  Finished := False;
+  FPastFirstItem := False;
+  FFinished := False;
 
-  DosCommand.CurrentDir := BDSRootPath(SelectedBDSVersion);
-  DosCommand.CommandLine := 'GetItCmd.exe -l= -s=name -f=all';
+  case rgrpSortBy.ItemIndex of
+    0: SortField := 'name';
+    1: SortField := 'vendor';
+    2: SortField := 'date';
+  end;
+
+  DosCommand.CurrentDir := 'C:\Program Files (x86)\Embarcadero\Studio\21.0\bin'; // BDSRootPath(SelectedBDSVersion);
+  DosCommand.CommandLine := Format('GetItCmd.exe -l=%s --sort=% --filter=%s',
+                    [edtNameFilter.Text, SortField, IfThen(chkInstalledOnly.Checked, 'installed', 'all')]);
 
   Screen.Cursor := crHourGlass;
   try
-    DosCommand.Execute;
+    var CmdTime: TStopWatch;
+    CmdTime.Start;
 
+    DosCommand.Execute;
     repeat
       Application.ProcessMessages;
-    until Finished;
+    until FFinished;
+    CleanPackageList;
+
+    cmdTime.Stop;
+    DownloadTime := cmdTime.Elapsed.Seconds;
+    PackageCount := lbPackages.Items.Count;
   finally
     Screen.Cursor := crDefault;
   end;
@@ -97,21 +128,37 @@ begin
   end;
 end;
 
+procedure TfrmAutoGetItMain.CleanPackageList;
+{ not sure if there's a bug in DosCommand or what but the list of packages
+  often contains cut-off entries that are then completed on the next line,
+  so this routine goes through and deletes those partial entries }
+var
+  LastPackage: string;
+begin
+  LastPackage := EmptyStr;
+  for var i := lbPackages.Count - 1 downto 1 do begin
+    LastPackage := lbPackages.Items[i-1];
+
+    if (LastPackage.Length > 0) and StartsText(LastPackage, lbPackages.Items[i]) then
+      lbPackages.Items.Delete(i - 1);
+  end;
+end;
+
 procedure TfrmAutoGetItMain.DosCommandNewLine(ASender: TObject; const ANewLine: string; AOutputType: TOutputType);
 begin
-  if not PastFirstItem then begin
+  if not FPastFirstItem then begin
     if StartsText('--', ANewLine) then
-      PastFirstItem := True;
-  end else if Length(Trim(ANewLine)) = 0 then
-    Finished := True
-  else if not Finished then
+      FPastFirstItem := True;
+  end else if ContainsText(ANewLine, 'command finished') then
+    FFinished := True
+  else if not FFinished and (Trim(ANewLine).Length > 0) then
     if lbPackages.Items.IndexOf(ANewLine) = -1 then
       lbPackages.Items.Add(ANewLine);
 end;
 
 procedure TfrmAutoGetItMain.DosCommandTerminated(Sender: TObject);
 begin
-  Finished := True;
+  FFinished := True;
 end;
 
 procedure TfrmAutoGetItMain.LoadRADVersionsCombo;
@@ -151,9 +198,16 @@ begin
   Result := Trim(Copy(cmbRADVersions.Text, 1, Pos(' ', cmbRADVersions.Text) - 1));
 end;
 
-procedure TfrmAutoGetItMain.FormCreate(Sender: TObject);
+procedure TfrmAutoGetItMain.SetDownloadTime(const Value: Integer);
 begin
-  LoadRADVersionsCombo;
+  StatusBar.Panels[1].Text := Format('%d seconds', [Value]);
+  StatusBar.Update;
+end;
+
+procedure TfrmAutoGetItMain.SetPackageCount(const Value: Integer);
+begin
+  StatusBar.Panels[0].Text := Format('%d packages', [Value]);
+  StatusBar.Update;
 end;
 
 end.
